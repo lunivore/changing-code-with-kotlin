f506edd4878b555ca0fe63ddddc67298e885c2c3
Darren Hobbs <darren.hobbs@gmail.com>
2018-10-07 18:19:39 +0100

    extensions example

---
+++ src/test/kotlin/extensions/StringsTest.kt
@@ -0,0 +1,21 @@
+package extensions
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class StringsTest {
+
+    @Test
+    fun `strings should be able to tell us if they are palindromes`() {
+//        assertTrue("bob".isPalindrome())
+//        assertFalse("darren".isPalindrome())
+//        assertFalse(null.isPalindrome())
+//        assertTrue("".isPalindrome())
+//        assertTrue("Bob".isPalindrome())
+    }
+}
+
+private fun String?.isPalindrome(): Boolean {
+    return this?.let { reversed().equals(this, true) } ?: false
+}
