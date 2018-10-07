a8c270196079fc5273f8ad15102f7a7a50cf0d5d
Darren Hobbs <darren.hobbs@gmail.com>
2018-10-07 18:28:16 +0100

    extensions example all cases covered and comments

--- src/test/kotlin/extensions/StringsTest.kt
+++ src/test/kotlin/extensions/StringsTest.kt
@@ -8,15 +8,20 @@ class StringsTest {
 
     @Test
     fun `strings should be able to tell us if they are palindromes`() {
+        // simple palindrome
         assertTrue("bob".isPalindrome)
+        // not a palindrome
         assertFalse("darren".isPalindrome)
+        // edge case, let's say the empty string is a palindrome
         assertTrue("".isPalindrome)
+        // but null is not
         assertFalse(null.isPalindrome)
-//        assertTrue("Bob".isPalindrome)
+        // ignore case
+        assertTrue("Bob".isPalindrome)
     }
 }
 
 private val String?.isPalindrome: Boolean get() {
     if (this == null) return false
-    return this.reversed() == this
+    return this.reversed().equals(this, true)
 }
